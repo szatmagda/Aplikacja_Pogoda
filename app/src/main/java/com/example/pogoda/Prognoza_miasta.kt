cceafd58bc751153a99fc0ba73aaa3c849dedce5
szatmagda <szatmagda@gmail.com>
2025-06-12 14:18:53 +0200

    Poprawki

--- app/src/main/java/com/example/pogoda/Prognoza_miasta.kt
+++ app/src/main/java/com/example/pogoda/Prognoza_miasta.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -88,8 +89,3 @@ class Prognoza_miasta : AppCompatActivity() {
         }
     }
 }
-
-
-
-
-
